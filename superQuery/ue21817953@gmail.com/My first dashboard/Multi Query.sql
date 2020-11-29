943c9246fd0a2b3b6ac50441380b34a5c3accc6d
javi600 <71374660+javi600@users.noreply.github.com>
2020-11-29 02:11:28 +0100

    Auto-commit: Sun Nov 29 2020 02:11:24 GMT+0100 (hora estándar de Europa central)

---
+++ superQuery/ue21817953@gmail.com/My first dashboard/Multi Query.sql
@@ -0,0 +1,8 @@
+/*Run multiple queries in parallel or in a sequence.
+Just separate each query with a semicolon, like in the example below.*/
+
+SELECT 1;
+SELECT 2;
+SELECT Query 2;
+SELECT Query 1;
+SELECT 9;
